01ede3ca9c7fec0cf6e080f895fc60fab0894335
jjhkie <83710866+jjhkie@users.noreply.github.com>
2022-08-29 13:58:32 +0900

    [ UI 구성 ] 기본 화면 구성

---
+++ COVID19/CovidDetailViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
